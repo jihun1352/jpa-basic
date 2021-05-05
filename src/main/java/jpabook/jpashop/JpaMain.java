63d56d4ad20830e81be392bbe0a47a3c1c5d1141
jihun <jihun5008@naver.com>
2021-05-05 15:51:06 +0900

    study

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,9 +1,13 @@
-package hellojpa;
+package jpabook.jpashop;
+
+import hellojpa.Member;
+import hellojpa.RoleType;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -15,19 +19,6 @@ public class JpaMain {
         tx.begin();
 
         try {
-            //영속
-            Member member1 = new Member();
-            Member member2 = new Member();
-            member1.setName("A");
-            member1.setRoleType(RoleType.ADMIN);
-
-            member2.setName("B");
-            member2.setRoleType(RoleType.USER);
-
-            em.persist(member1);
-            em.persist(member2);
-            System.out.println("member1.getId() = " + member1.getId());
-            System.out.println("===================================");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
