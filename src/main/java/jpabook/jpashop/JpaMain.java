f09a8e062fef0e3aa4e817e363dc9176a69cef47
jihun1352 <jihun5008@naver.com>
2021-05-08 00:03:32 +0900

    study

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -2,6 +2,8 @@ package jpabook.jpashop;
 
 import hellojpa.RoleType;
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.Team;
 
 import javax.persistence.EntityManager;
@@ -21,32 +23,8 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
-
-            // Member이 주인이기 때문에 setTeam(team)을 넣어준다.
-            Member member = new Member();
-            member.setUsername("jpa");
-            member.changeTeam(team);
-            em.persist(member);
-
-            //연관관계 편의 메소드로 member.setTeam(team);에서 team에도 같이 들어가기 때문에
-            //아래 코드는 없어도 된다.
-            //team.getMembers().add(member);
-
-            //em.flush();
-            //em.clear();
-
-            // flush, clear을 안하면 em.find시 위의 em.persist로 인해 1차 캐쉬에 저장되어 있어서
-            // 메모리상에서 조회를 하기 때문에 값이 없다.
-            // member1의 사이즈가 0이 된다.
-            Team team1 = em.find(Team.class, team.getId());
-            List<Member> member1 = team1.getMembers();
-            System.out.println("member1.size() = " + member1.size());
-            for (Member m : member1) {
-                m.getUsername();
-            }
+            Order order = new Order();
+            order.addOrderItem(new OrderItem());
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Member {
@@ -16,6 +18,9 @@ public class Member {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "TEAM_ID")
     private Team team;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
 
     public Long getId() {
         return id;
